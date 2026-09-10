af9026cee325d3bdcca6832210ce93931d6ebbb5
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-10 15:15:57 +0500

    first Push

---
+++ React With Js/src/Add.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Add() {
+  return (
+   <>
+     <h1>This can plus two numbers</h1>
+      console.log({5+5});
+   </>
+  )
+}
+
+export default Add
---
+++ React With Js/src/App.jsx
@@ -0,0 +1,26 @@
+class StudentInfo{
+  constructor(name,age,city){
+    this.studentName = name;
+    this.age = age;
+    this.city = city;
+  };
+};
+
+
+const std1 = new StudentInfo("Misam Shaban" , 23 , "Sahiwal");
+const std2 = new StudentInfo("Ahmad Shaban" , 18 , "Sahiwal" );
+
+
+
+
+function App() {
+  return (
+   <>
+   {Object.values(std2)}
+   {Object.values(std1)}
+   </>
+  );
+};
+
+export default App;
+

5715dc4168d0d0b93365fa57b37050af132d5ac5
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-15 01:52:55 +0500

    Spread Operator

---
+++ spreed.jsx
@@ -0,0 +1,18 @@
+const spreadOne = [1,2,3,4,5];
+const spreadTwo = [6,7,8,9,10];
+
+const collectData = {...spreadOne, ...spreadTwo };
+
+import React from 'react'
+
+function Spreed() {
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default Spreed;

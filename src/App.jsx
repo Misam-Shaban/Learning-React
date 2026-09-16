c8ba40b5c0cbe9e3172100c6e5357cc7fb048e88
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-17 00:49:37 +0500

    Children Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // import ArrowFuucWithThis from "./ArrowFunc.jsx";
 // import First from "./Components/first.jsx";
-import Newcard from "./Components/Newcard.jsx";
+import Card from "./Card.jsx";
+import Chlidrenprops from "./Components/Chlidrenprops.jsx";
+// import Newcard from "./Components/Newcard.jsx";
 // import Extands from "./src./extands.jsx";
 // import Destructring from "../destructring.jsx";
 // import Map from "./map.jsx";
@@ -16,8 +18,14 @@ function App() {
    {/* <Destructring/> */}
    {/* <Spreed/> */}
    {/* <First/> */}
-   <Newcard studentName="Misam Shaban" course="React Js" age={24} city="Lahore" aboutLeaning="Currently building projects with React JS" />
-   <Newcard studentName="Ahmad Shaban" course="JavaScript" age={18} city="Sahiwal" aboutLeaning="Currently Learning basics of JS"  />
+   {/* <Newcard studentName="Misam Shaban" course="React Js" age={24} city="Lahore" aboutLeaning="Currently building projects with React JS" />
+   <Newcard studentName="Ahmad Sha
+   ban" course="JavaScript" age={18} city="Sahiwal" aboutLeaning="Currently Learning basics of JS"  /> */}
+   <Chlidrenprops>
+    <h1>what is your Name</h1>
+    <h2>My Name is Misam Shaban..</h2>
+    <Card/>
+   </Chlidrenprops>
    </>
   );
 };
---
+++ src/Components/Chlidrenprops.jsx
@@ -0,0 +1,14 @@
+
+
+
+function Chlidrenprops({children}) {
+  return (
+    <>
+      <div>
+      {children}
+      </div>
+    </>
+  )
+}
+
+export default Chlidrenprops;

cc6f6aa1149b0b2b47b5a17b1aabec75c8d6a6c2
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-19 01:25:19 +0500

    Mounting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(50);
+
   useEffect(() => {
-    console.log("Re Render howa ha");
+    console.log("Mounting...");
   });
-  return <>App Component</>;
+  return (
+    <>
+      App Component
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>increment</button>
+      <h1>{data}</h1>
+      <button onClick={() => setData(data - 1)}>decrement</button>
+    </>
+  );
 }
 
 export default App;
